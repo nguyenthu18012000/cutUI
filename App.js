87ee48b851e73c7be14541acf53e159149fa1bb9
nguyenthu18012000 <nguyenthu180100@gmail.com>
2022-10-09 22:25:48 +0700

    third

--- App.js
+++ App.js
@@ -10,38 +10,40 @@ import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/AntDesign';
 import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconFA from 'react-native-vector-icons/FontAwesome5';
+import SvgUri from 'react-native-svg-uri';
+import Svg from 'react-native-svg';
+import card from './assets/images/cardback.svg'
 
 const pi = Math.PI;
 const screenWidth = Dimensions.get('window').width;
 const ringRadius = screenWidth * 0.5 / 2;
-const array = [1,2,3,4,5,6,7,8,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 const numberOfCard = array.length;
 const degree = 360 / numberOfCard;
 const widthCard = 85.85;
-const heightCard = 112.61;
+const heightCard = 121.61;
 const padding = (screenWidth / 2 - ringRadius);
-console.log(padding, screenWidth / 2, ringRadius)
 
 const App = () => {
 
   const convertDegreeToRadiant = (degree) => {
-    return (degree/180) * pi;
+    return (degree / 180) * pi;
   }
 
   const getTopPosition = (degree) => {
     const radiant = convertDegreeToRadiant(degree);
     const sin = Math.sin(radiant);
-    return (1 - sin) * ringRadius - heightCard / 2;
+    return (1 - sin) * ringRadius - heightCard / 2 + Math.random() * 5 ;
   }
 
   const getLeftPosition = (degree) => {
     const radiant = convertDegreeToRadiant(degree);
     const cos = Math.cos(radiant);
-    return (1 + cos) * ringRadius - widthCard / 2;
+    return (1 - cos) * ringRadius - widthCard / 2;
   }
 
   const getRotate = (index, degree) => {
-    return (- index * degree + 90) + 'deg';
+    return ( index * degree - 90 + Math.random() * 5) + 'deg';
   }
 
   return (
@@ -52,35 +54,67 @@ const App = () => {
       >
         <View style={styles.header}>
           <View>
-            <Icon name='close' style={styles.icon} />
+            <Image source={require('./assets/images/X.png')} />
           </View>
           <View>
-            <Text style={styles.headerTitle}>King's Cup</Text>
+            <Text style={styles.headerTitle}>
+              {/* King's Cup */}
+            </Text>
           </View>
           <View>
-            <IconMaterial name='chat-question-outline' style={styles.icon} />
+            <Image source={require('./assets/images/question.png')} />
           </View>
         </View>
         <View style={styles.container}>
           <View style={styles.layout}>
             <View style={styles.orderRing}>
               {array.map((item, key) => (
+                // <Image
+                //   key={key}
+                //   source={require('./assets/images/cardback.svg')}
+                //   style={
+                //      [
+                //       {
+                //         left: getLeftPosition(key * degree),
+                //         top: getTopPosition(key * degree),
+                //         transform: [{ rotate: getRotate(key, degree) }]
+                //       },
+                //       styles.orderItem
+                //     ]
+                //   }
+                // />
+                // <SvgUri key={key}
+                // alt='lksdjflaksjdflks'
+                //   style={
+                //     [
+                //       {
+                //         left: getLeftPosition(key * degree),
+                //         top: getTopPosition(key * degree),
+                //         transform: [{ rotate: getRotate(key, degree) }]
+                //       },
+                //       styles.orderItem
+                //     ]
+                //   }
+                //   svgXmlData={'./assets/images/cardback.png'}
+                // />
+                <Svg key={key}>
                   <Image
-                  key={key}
-                  source={require('./assets/images/cardback.png')}
-                  style={
-                    [
-                      {
-                        left:getLeftPosition(key * degree),
-                        top: getTopPosition(key * degree),
-                        transform: [{ rotate: getRotate(key, degree) }]
-                      },
-                      styles.orderItem
-                    ]
-                  }
-                />)
-              )}
-            
+
+                    source={require('./assets/images/cardback.svg')}
+                    style={
+                      [
+                        {
+                          left: getLeftPosition(key * degree),
+                          top: getTopPosition(key * degree),
+                          transform: [{ rotate: getRotate(key, degree) }]
+                        },
+                        styles.orderItem
+                      ]
+                    }
+                  />
+                </Svg>
+              ))}
+
             </View>
           </View>
 
@@ -95,11 +129,11 @@ const App = () => {
         <View style={styles.infoGame}>
           <View></View>
           <View style={styles.content}>
-            <IconMaterial name='cards-playing-diamond-outline' style={styles.cardIcon} />
+            <Image source={require('./assets/images/card.png')} />
             <Text style={styles.cardNumber}>50</Text>
           </View>
           <View style={styles.content}>
-            <IconFA name='crown' style={styles.crownIcon} />
+          <Image source={require('./assets/images/crown.png')}  />
             <Text style={styles.cardNumber}>1</Text>
           </View>
           <View></View>
@@ -130,7 +164,7 @@ const styles = StyleSheet.create({
   },
   icon: {
     fontSize: 20,
-    color: 'white'
+    color: '#F5F3F3'
   },
   container: {
     width: screenWidth,
@@ -163,20 +197,22 @@ const styles = StyleSheet.create({
   },
   dropSide: {
     backgroundColor: 'rgba(255, 255, 255, 0.1)',
-    height: 150,
-    width: 120,
+    height: 155,
+    width: 130,
     alignItems: 'center',
     justifyContent: 'center',
     paddingLeft: 20,
     paddingRight: 20,
-    borderColor: 'white',
+    borderColor: 'rgba(255, 255, 255, 0.3)',
     borderWidth: 1,
     borderStyle: 'dashed',
     borderRadius: 5
   },
   dropText: {
-    fontSize: 20,
-    color: 'white'
+    fontSize: 22,
+    color: '#FFFFFF',
+    opacity: 0.6,
+    alignItems: 'center'
   },
   infoGame: {
     margin: 15,
@@ -188,16 +224,17 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
   cardIcon: {
-    color: 'white',
-    fontSize: 30
+    width: 15.59,
+    height: 22.47,
+    justifyContent: 'center'
   },
   cardNumber: {
     color: 'white',
-    padding: 6
+    paddingLeft: 10
   },
   crownIcon: {
-    color: 'white',
-    fontSize: 25
+    color: 'yellow',
+    fontSize: 20
   },
 });
 
