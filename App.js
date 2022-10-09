45c08d67db98dc985ffb1081b9dc9e5d62739ff6
nguyenthu18012000 <nguyenthu180100@gmail.com>
2022-10-09 09:17:06 +0700

    second

--- App.js
+++ App.js
@@ -11,9 +11,38 @@ import Icon from 'react-native-vector-icons/AntDesign';
 import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconFA from 'react-native-vector-icons/FontAwesome5';
 
+const pi = Math.PI;
 const screenWidth = Dimensions.get('window').width;
+const ringRadius = screenWidth * 0.5 / 2;
+const array = [1,2,3,4,5,6,7,8,9,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+const numberOfCard = array.length;
+const degree = 360 / numberOfCard;
+const widthCard = 85.85;
+const heightCard = 112.61;
+const padding = (screenWidth / 2 - ringRadius);
+console.log(padding, screenWidth / 2, ringRadius)
 
 const App = () => {
+
+  const convertDegreeToRadiant = (degree) => {
+    return (degree/180) * pi;
+  }
+
+  const getTopPosition = (degree) => {
+    const radiant = convertDegreeToRadiant(degree);
+    const sin = Math.sin(radiant);
+    return (1 - sin) * ringRadius - heightCard / 2;
+  }
+
+  const getLeftPosition = (degree) => {
+    const radiant = convertDegreeToRadiant(degree);
+    const cos = Math.cos(radiant);
+    return (1 + cos) * ringRadius - widthCard / 2;
+  }
+
+  const getRotate = (index, degree) => {
+    return (- index * degree + 90) + 'deg';
+  }
 
   return (
     <View>
@@ -32,35 +61,26 @@ const App = () => {
             <IconMaterial name='chat-question-outline' style={styles.icon} />
           </View>
         </View>
-        <View >
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-
-            <View
-              style={{
-                borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
-                width: Dimensions.get('window').width * 0.8,
-                height: Dimensions.get('window').width * 0.8,
-                borderWidth: 0,
-                borderColor: "none",
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-              underlayColor='#ccc'
-            >
-              <Image
-                source={require('./assets/images/cardback.png')}
-                style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.5, transform: [{rotate: '30deg'}] }}
-              />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.7 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.7 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.7 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.6, right: 20 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.6, left: 20 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", top: Dimensions.get('window').width * 0.7 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", top: Dimensions.get('window').width * 0.6, right: 20 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", top: Dimensions.get('window').width * 0.6, left: 20 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.3, left: -20 }} />
-              <Image source={require('./assets/images/cardback.png')} style={{ height: 50, width: 50, position: "absolute", bottom: Dimensions.get('window').width * 0.3, right: -20 }} />
+        <View style={styles.container}>
+          <View style={styles.layout}>
+            <View style={styles.orderRing}>
+              {array.map((item, key) => (
+                  <Image
+                  key={key}
+                  source={require('./assets/images/cardback.png')}
+                  style={
+                    [
+                      {
+                        left:getLeftPosition(key * degree),
+                        top: getTopPosition(key * degree),
+                        transform: [{ rotate: getRotate(key, degree) }]
+                      },
+                      styles.orderItem
+                    ]
+                  }
+                />)
+              )}
+            
             </View>
           </View>
 
@@ -112,6 +132,31 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'white'
   },
+  container: {
+    width: screenWidth,
+    height: screenWidth
+  },
+  layout: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  orderRing: {
+    borderRadius: Math.round(screenWidth) / 2,
+    width: ringRadius * 2,
+    height: ringRadius * 2,
+    borderWidth: 0,
+    borderColor: "red",
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative'
+  },
+  orderItem: {
+    height: heightCard,
+    width: widthCard,
+    position: "absolute",
+    backgroundColor: '#f0b6bc'
+  },
   dropArea: {
     flexDirection: 'row',
     justifyContent: 'space-between'
@@ -124,7 +169,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingLeft: 20,
     paddingRight: 20,
-    // opacity: 0.1,
     borderColor: 'white',
     borderWidth: 1,
     borderStyle: 'dashed',
@@ -132,7 +176,7 @@ const styles = StyleSheet.create({
   },
   dropText: {
     fontSize: 20,
-    color: 'gray'
+    color: 'white'
   },
   infoGame: {
     margin: 15,
